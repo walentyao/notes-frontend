b39b167034d0fe53e872e94dda2fa8166b3b8d17
walentyao <prikolnyyyvalk@yandex.ru>
2024-02-18 01:36:08 +0300

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+    return (
+        <>
+            <div>ku</div>
+        </>
+    );
+}
+
+export default App;
---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,4 @@
+import classes from './ErrorPage.module.scss';
+export const ErrorPage = () => {
+    return <div className={classes.error}>Уууу сука</div>;
+};
